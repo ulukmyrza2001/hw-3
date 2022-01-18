dda741d8e64a419cbdcff7becf2df76ee609e772
ulukmyrza2001 <ulukmyrzazhanybekov01@gmail.com>
2022-01-18 15:58:27 +0600

    2-version бутту

--- src/components/Inputform.jsx
+++ src/components/Inputform.jsx
@@ -1,28 +1,68 @@
-import React,{ useState } from 'react'
+import React, { useState } from 'react'
+import List from './List'
 
 const Inputform = () => {
-    const [task,setTask] = useState('')
-    const [tasks,setTasks] = useState('')
-    const AddTaskHandler = (e) =>{
-       setTask(e.target.value)
-    }
-    const onAddHandler = (e) =>{
-        e.preventDefault()
-        // const newTask = task;
-        setTasks([...tasks,task])
-    }
-    console.log(tasks);
-	return <form onSubmit={onAddHandler} >
-        <input type="text" className="tasks" onChange={AddTaskHandler} value={task} />
-        <button type='submit' className="add">Add</button>
-        <ul>
-            {setTasks(tasks.map(el=>{
-                return(
-                    <li>{el}</li>
-                )
-            }))}
-        </ul>
-    </form>
+	const [tasks, setTasks] = useState([])
+	const [task, setTask] = useState({
+		fname: '',
+		lname: '',
+		email: '',
+	})
+	const AddTaskHandler = (e) => {
+		const name = e.target.name
+		setTask({
+			...task,
+			[name]: e.target.value,
+		})
+	}
+	const onAddHandler = (e) => {
+		e.preventDefault()
+		// const newTask = task;
+		setTasks([...tasks, task])
+	}
+	console.log(tasks)
+
+	return (
+		<div>
+			<form onSubmit={onAddHandler}>
+				<div>
+					<label>First name</label>
+					<input
+						name='fname'
+						type='text'
+						className='fname'
+						onChange={AddTaskHandler}
+						value={task.fname}
+					/>
+				</div>
+				<div>
+					<label>Last name</label>
+					<input
+						name='lname'
+						type='text'
+						className='lname'
+						onChange={AddTaskHandler}
+						value={task.lname}
+					/>
+				</div>
+				<div>
+					<label htmlFor=''>Email</label>
+					<input
+						name='email'
+						type='text'
+						className='email'
+						onChange={AddTaskHandler}
+						value={task.email}
+					/>
+				</div>
+
+				<button type='submit' className='add'>
+					Add
+				</button>
+			</form>
+            <List data = {tasks}/>
+		</div>
+	)
 }
 
 export default Inputform
