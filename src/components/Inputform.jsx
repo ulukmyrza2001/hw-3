7027b9a013f2b6fe4806d2f827540657fbf94942
ulukmyrza2001 <ulukmyrzazhanybekov01@gmail.com>
2022-01-18 17:18:28 +0600

    first version

--- src/components/Inputform.jsx
+++ src/components/Inputform.jsx
@@ -1,28 +1,78 @@
-import React,{ useState } from 'react'
+import React, { useState } from 'react'
+import List from './List'
+import './InputForm.css'
 
 const Inputform = () => {
-    const [task,setTask] = useState('')
-    const [tasks,setTasks] = useState('')
-    const AddTaskHandler = (e) =>{
-       setTask(e.target.value)
-    }
-    const onAddHandler = (e) =>{
-        e.preventDefault()
-        // const newTask = task;
-        setTasks([...tasks,task])
-    }
-    console.log(tasks);
-	return <form onSubmit={onAddHandler} >
-        <input type="text" className="tasks" onChange={AddTaskHandler} value={task} />
-        <button type='submit' className="add">Add</button>
-        <ul>
-            {setTasks(tasks.map(el=>{
-                return(
-                    <li>{el}</li>
-                )
-            }))}
-        </ul>
-    </form>
+	const [fname, setFname] = useState('')
+	const [lname, setLname] = useState('')
+	const [email, setEmail] = useState('')
+	const [tasks, setTasks] = useState([])
+	const addFnameHandler = (e) => {
+		setFname(e.target.value)
+	}
+	const addLnameHandler = (e) => {
+		setLname(e.target.value)
+	}
+	const addEmailHandler = (e) => {
+		setEmail(e.target.value)
+	}
+	const onAddHandler = (e) => {
+		e.preventDefault()
+		const newData = {
+			fname: fname,
+			lname: lname,
+			email: email,
+		}
+		// const newTask = task;
+		setTasks([...tasks, newData])
+        console.log(tasks);
+	}
+	return (
+		<div>
+			<div className='center'>
+				<form onSubmit={onAddHandler}>
+					<h1>Our Newsletter</h1>
+					<div className='inputbox'>
+						<input
+							name='fname'
+							type='text'
+							className='fname'
+							required='required'
+							onChange={addFnameHandler}
+							value={fname}
+						/>
+						<span>First name</span>
+					</div>
+					<div className='inputbox'>
+						<input
+							name='lname'
+							type='text'
+							className='lname'
+							required='required'
+							onChange={addLnameHandler}
+							value={lname}
+						/>
+						<span>Last name</span>
+					</div>
+					<div className='inputbox'>
+						<input
+							name='email'
+							type='text'
+							className='email'
+							required='required'
+							onChange={addEmailHandler}
+							value={email}
+						/>
+						<span>Email</span>
+					</div>
+					<div className='inputbox'>
+						<input type='submit' value='submit' />
+					</div>
+				</form>
+			</div>
+            <List data={tasks} />
+		</div>
+	)
 }
 
 export default Inputform
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
-
-const List = () => {
-    return (
-        <div>
-             <ul>
-				{props.data.map((el, id) => {
-					return (
-						<div key={id}>
-							<h2 className='fnameShow'>{el.fname}</h2>
-							<h3 className='lnameShow'>{el.lname}</h3>
-							<h1 className='emailShow'>{el.email}</h1>
-						</div>
-					)
-				})}
-			</ul>
-        </div>
-    )
+import './list.css'
+const List = (props) => {
+	return (
+		<div className='list'>
+			{props.data.map((el, id) => {
+				return (
+					<div key={id}>
+						<h2 className='fnameShow'>{el.fname}</h2>
+						<h3 className='lnameShow'>{el.lname}</h3>
+						<h1 className='emailShow'>{el.email}</h1>
+					</div>
+				)
+			})}
+		</div>
+	)
 }
 
 export default List
